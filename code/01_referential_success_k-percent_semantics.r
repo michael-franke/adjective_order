896c5415fae7261eb68f7113dccf9fca616240e9
michael-franke <mchfranke@gmail.com>
2019-01-20 17:12:44 +0100

    added figure to paper

--- code/01_referential_success_k-percent_semantics.r
+++ code/01_referential_success_k-percent_semantics.r
@@ -98,7 +98,7 @@ get_outcomes = function(iterations, n_obj = 6, sd_brown = 0.1, sd_tall = 0.1, th
 ## collect results in a tibble ::: 
 #### ideally, for each parameter tuple, "tall_brown" should have a higher mean_success than "brown_tall"
 
-iterations = 5000
+iterations = 50
 results = rbind(
   get_outcomes(iterations, n_obj = 4, sd_brown = 0.1, sd_tall = 0.25, theta = 0.5),
   get_outcomes(iterations, n_obj = 6, sd_brown = 0.1, sd_tall = 0.25, theta = 0.5),
@@ -115,7 +115,8 @@ results = rbind(
 )
 show(results)
 
-results_plot = select(filter(results, context_types == "all"), -sum_success_prob) %>% spread(key = ordering, value = mean_success) %>%
+results_plot = select(filter(results, context_types == "all"), -sum_success_prob) %>% 
+  spread(key = ordering, value = mean_success) %>%
   ggplot(aes(x = as.factor(n_obj), y = tall_brown - brown_tall, fill = as.factor(theta))) +
   geom_bar(stat = "identity", position = "dodge") + facet_grid(sd_brown ~ sd_tall) +
   ylab("difference in mean success probability") +
