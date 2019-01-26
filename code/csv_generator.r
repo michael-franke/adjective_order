6a95ba9011a407c050a0e95abb1322fdb33d5763
Mihael Simonič <mihael.simonic@ijs.si>
2019-01-26 01:28:28 +0100

    implemented truncating and minmax for ground values

--- code/csv_generator.r
+++ code/csv_generator.r
@@ -7,10 +7,11 @@
 
 library(tidyverse)
 library(HDInterval)
+library(truncdist)
 
 ## convenience functions
 
-get_agent_representation = function(true_values, n_obj, sd_brown, sd_tall) {
+get_agent_representation = function(true_values, n_obj, sd_brown, sd_tall, bounded_dist_behaviour = "norm") {
   out = matrix(c(rnorm(n_obj, mean = true_values[1,], sd_brown),
                  rnorm(n_obj, mean = true_values[2,], sd_tall)), 
                nrow = 2, byrow = TRUE)
@@ -18,14 +19,6 @@ get_agent_representation = function(true_values, n_obj, sd_brown, sd_tall) {
   rownames(out) = c("brown", "tall")
   out
 }
-
-# get_truth_values = function(representation, theta) {
-#   out = rank(as.vector(representation)) > ceiling(length(representation) * theta)
-#   # at least the maximal element has the property in question
-#   if (sum(out) == 0) {out[which.max(as.vector(representation))] = TRUE} 
-#   names(out) = names(representation)
-#   out
-# }
 
 get_truth_values = function(representation, theta) {
   out = representation > (max(representation) - min(representation) * theta) + min(representation)
@@ -35,18 +28,32 @@ get_truth_values = function(representation, theta) {
   out
 }
 
+get_ground_value_for_adj = function(n_obj, adj_dist_type, norm_dist_behaviour) {
+  out = switch(adj_dist_type, #adjective is normally distributed 
+                              norm={switch(norm_dist_behaviour,
+                                                 #normal dist without boundaries
+                                                 norm = rnorm(n_obj),
+                                                 #bounded "normal" dist
+                                                 trunc = rtrunc(n_obj, spec="norm", a=0, b=1),
+                                                 minmax = pmin(pmax(rnorm(n_obj),0),1))},
+                              #beta(1,1)
+                              beta_1_1={rbeta(n_obj, shape1 = 1, shape2 = 1)},
+                              #beta(1,5)
+                              beta_1_5={rbeta(n_obj, shape1 = 1, shape2 = 5)}
+  )
+  out
+}
+  
+
 ## main function to get an outcome for given parameter settings
 
-get_outcome = function(n_obj, sd_brown, sd_tall, theta, ground_truth_dist_brown,  ground_truth_dist_tall, bounded_dist_behaviour = "not implemented") {
+get_outcome = function(n_obj, sd_brown, sd_tall, theta, ground_truth_dist_brown,  ground_truth_dist_tall, norm_dist_behaviour = "norm") {
 
-
-    true_values = matrix(c( switch(ground_truth_dist_brown, norm={rnorm(n_obj)}, beta_1_1={rbeta(n_obj, shape1 = 1, shape2 = 1)}, beta_1_5={rbeta(n_obj, shape1 = 1, shape2 = 5)}), 
-                            switch(ground_truth_dist_brown, norm={rnorm(n_obj)}, beta_1_1={rbeta(n_obj, shape1 = 1, shape2 = 1)}, beta_1_5={rbeta(n_obj, shape1 = 1, shape2 = 5)}) 
-                           ), 
-                         nrow = 2,
-                         byrow = TRUE)
+    true_values = matrix(
+      c(get_ground_value_for_adj(n_obj, ground_truth_dist_brown, norm_dist_behaviour), get_ground_value_for_adj(n_obj, ground_truth_dist_brown, norm_dist_behaviour)), 
+      nrow = 2,
+      byrow = TRUE)
     
-    # TODO: bounded_dist_behaviour
     speaker_representation = get_agent_representation(true_values, n_obj, sd_brown, sd_tall) 
     listener_representation = get_agent_representation(true_values, n_obj, sd_brown, sd_tall) 
     
@@ -68,7 +75,8 @@ get_outcome = function(n_obj, sd_brown, sd_tall, theta, ground_truth_dist_brown,
            tall_brown = ifelse(referent %in% tall_brown, 1/length(tall_brown), 0),
            brown_tall = ifelse(referent %in% brown_tall, 1/length(brown_tall), 0),
            referent = referent,
-           exception = tall_brown < brown_tall,
+           success = tall_brown > brown_tall,
+           exception = !success,
            sd_adjs_equal = sd_tall == sd_brown,
            num_referents_speaker = sum(tall_sp & brown_sp),
            num_referents_listener = sum(tall_li & brown_li),
@@ -80,23 +88,19 @@ get_outcome = function(n_obj, sd_brown, sd_tall, theta, ground_truth_dist_brown,
            sd_tall = sd_tall, 
            theta = theta, 
            ground_truth_dist_brown = ground_truth_dist_brown, 
-           ground_truth_dist_tall = ground_truth_dist_tall,
-           bounded_dist_behaviour = bounded_dist_behaviour
+           ground_truth_dist_tall = ground_truth_dist_tall
            )
 }
 
 
 distributions = c("norm", "beta_1_1", "beta_1_5")
-closed_dist_behavior= c("map_to_boundary", "truncated_dist")
+iterations = 5*10^6
 
-
-iterations = 50
-data = list()
-
+data_norm = list(); data_trunc = list(); data_minmax = list();
 for (i in 1:iterations) {
   nobj = sample(4:20,1)
   
-  sd_adjs = runif(2, 0.1, 0.5)
+  sd_adjs = runif(2, 0.1, 0.4)
   sd_adj1 = min(sd_adjs) # we want to make sure that the first adj is less subjective
   sd_adj2 = max(sd_adjs) 
   
@@ -105,13 +109,20 @@ for (i in 1:iterations) {
   ground_dist_adj1 = distributions[sample(1:3,1)]
   ground_dist_adj2 = distributions[sample(1:3,1)]
   
-  dataline = get_outcome(n_obj = nobj, sd_brown = sd_adj1, sd_tall = sd_adj2, theta = threshold, ground_truth_dist_brown = ground_dist_adj1,  ground_truth_dist_tall = ground_dist_adj2)
+  #use of of three match_normal_dist_to_closed_dist_behaviors
+  #dataline = get_outcome(n_obj = nobj, sd_brown = sd_adj1, sd_tall = sd_adj2, theta = threshold, ground_truth_dist_brown = ground_dist_adj1,  ground_truth_dist_tall = ground_dist_adj2,"norm")
+  dataline = get_outcome(n_obj = nobj, sd_brown = sd_adj1, sd_tall = sd_adj2, theta = threshold, ground_truth_dist_brown = ground_dist_adj1,  ground_truth_dist_tall = ground_dist_adj2,"trunc")
+  #dataline = get_outcome(n_obj = nobj, sd_brown = sd_adj1, sd_tall = sd_adj2, theta = threshold, ground_truth_dist_brown = ground_dist_adj1,  ground_truth_dist_tall = ground_dist_adj2,"minmax")
   data[[i]] <- dataline
 }
 
 results=bind_rows(data)
 
+#filter out meaningless cases
+results = results %>% filter(! is.na(referent))
 
 # write csv
-write.csv(results, file = "big.csv")
+write.csv(results, file = "big+trunc+5M.csv")
 
+#print success
+print(sum(results$success)/nrow(results))
