227a76dbceeeb0dbdfd9928de4221f4400653378
michael-franke <mchfranke@gmail.com>
2019-01-28 09:26:43 +0100

    minor tweaks in text

--- code/04_analysis_mc_data.r
+++ code/04_analysis_mc_data.r
@@ -1,7 +1,8 @@
 mc_results = read_csv("03_mc_results.csv") %>% 
   mutate(EU_diff = tall_brown - brown_tall)
 
-regression_model = glm(EU_diff ~ n_obj + sd_brown + sd_tall + theta + dist_brown + dist_tall,
+model_saturated = glm(EU_diff ~ n_obj * sd_brown * sd_tall * theta * dist_brown * dist_tall,
                        data = mc_results)
 
-summary(regression_model)
+model_simpler = step(model_saturated)
+summary(model_simpler)
